56328b85b5f1dff661800adabca182ca9a8285c9
samson pallivathukkal <samps01@gmail.com>
2020-04-25 22:22:07 -0400

    Add max-sum-subarray

---
+++ algorithms/max-sum-subarray.js
@@ -0,0 +1,30 @@
+/**
+ * Find max sum subarray from the given Array
+ * Method 1 - Kadane's algorithm
+ **/
+
+
+let arr = [5, 7, -3, 2, 9, 6, 16, 22, 21, 29, -14, 10, 12];
+
+function maxSumSubArray(arr) {
+    if (arr.length == 0) {
+        return null;
+    }
+
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSumSubArray(arr));
+
+//TODO: Add method to return indices
+//TODO: Add test cases
+
+
+
