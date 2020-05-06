620f82c84fef0df51cc1e0e30601588e5ae6f3d9
samson pallivathukkal <samps01@gmail.com>
2020-05-06 18:41:08 -0400

    Add remove-duplicate-from-sorted-array

--- app/algorithms/array/__test__/array.test.js
+++ app/algorithms/array/__test__/array.test.js
@@ -1,6 +1,7 @@
 import { describe } from 'mocha';
 import { sortedSquaredArray, squareOf } from '../sorted-squared-array';
 import maxSumSubArray from '../max-sum-subarray';
+import removeDuplicates from "../remove-duplicates-from-sorted-array";
 
 const { assert, expect } = require('chai');
 
@@ -42,10 +43,42 @@ describe('Arrays', () => {
         });
 
         inputs.forEach((inputObj) => {
-            it(`should return sorted squared array of ${inputObj.output} for ${inputObj.input}`, () => {
+            it(`Should return sorted squared array of ${inputObj.output} for ${inputObj.input}`, () => {
                 const result = sortedSquaredArray(inputObj.input);
                 expect(result).to.eql(inputObj.output);
             });
         });
+    });
+
+    describe('remove-duplicates-from-sorted-Array', () => {
+       const inputs = [
+           {
+               input: [0,0,1,1,1,2,2,3,3,4],
+               output: {
+                   length: 5,
+                   modArr: [0,1,2,3,4]
+               }
+           },
+           {
+               input: [1,1,2],
+               output: {
+                   length: 2,
+                   modArr: [1,2]
+               }
+           }
+       ];
+
+       inputs.forEach(inputObj => {
+           it(`Should return length ${inputObj.output.length} for ${inputObj.input}`, () => {
+               const length = removeDuplicates(Array.from(inputObj.input));
+               expect(length).to.be.equal(inputObj.output.length);
+           });
+
+           it(`Should modify the array in place - ${inputObj.output.modArr} should be the first ${inputObj.output.length} elements`, () => {
+              const length = removeDuplicates(inputObj.input);
+              const resultArr = inputObj.input.slice(0, length);
+              expect(resultArr).to.eql(inputObj.output.modArr);
+           });
+       })
     });
 });
